39b5178645b682a2b915c80cf45e8ceb6a6d59fa
Aaxtrox <pawelw@gmx.com>
2022-11-14 10:21:09 +0000

    display player selection

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //prevent body from dragging
 document.body.ondragstart = function() { return false; };
 
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -9,3 +10,25 @@ let computerScore = 0;
 document.getElementById('playerScore').innerHTML = playerScore;
 //display computerScore in span id computerScore
 document.getElementById('computerScore').innerHTML = computerScore;
+
+
+//grab all buttons
+const buttons = document.querySelectorAll('button');
+//loop through buttons
+buttons.forEach((button) => {
+    //add event listener on each button
+    button.addEventListener('click', () => {
+        //grab id of clicked button
+        const id = button.id;
+        //if id = rock display rock image in div id playerChoice adn give id rock
+        if (id === 'rock') {
+            document.getElementById('playerChoice').innerHTML = '<img src="./img/rock.png" alt="rock" id="rock">';
+        } else if (id === 'paper') {
+            document.getElementById('playerChoice').innerHTML = '<img src="./img/paper.png" alt="paper" id="paper">';
+        } else if (id === 'scissors') {
+            document.getElementById('playerChoice').innerHTML = '<img src="./img/scissors.png" alt="scissors" id="scissors">';
+        }
+        //call game function
+        game();
+    });
+});

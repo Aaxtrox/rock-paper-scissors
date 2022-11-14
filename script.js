594168b9a944a715c0c65812da44da0925022821
Aaxtrox <pawelw@gmx.com>
2022-11-14 10:18:52 +0000

    display score

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //prevent body from dragging
 document.body.ondragstart = function() { return false; };
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+//display playerScore in span id playerScore
+document.getElementById('playerScore').innerHTML = playerScore;
+//display computerScore in span id computerScore
+document.getElementById('computerScore').innerHTML = computerScore;
